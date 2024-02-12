d79ff959928415a72f5300cb957f55de32be65c0
MdialloC19 <mdiallo3211@gmail.com>
2024-02-12 22:34:51 +0000

    styling ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./components/Header";
 function App() {
+  //   const title = "Blog Post";
+  //   const body = "This is my blog post";
+  //   const comments = [
+  //     { id: 1, text: "Comment one" },
+  //     { id: 2, text: "Comment two" },
+  //     { id: 3, text: "Comment three" },
+  //   ];
+
+  //   const loading = false;
+  //   const commentBlock = (
+  //     <div className="comments">
+  //       Comments({comments.length})
+  //       <ul>
+  //         {comments.map((comment, index) => (
+  //           <li key={index}>{comment.text}</li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   );
+  //   const showComment = true;
+  //   if (loading) return <h1>Loading...</h1>;
+  //   return (
+  //     <div className="container">
+  //       <h1>My app </h1>
+
+  //       {showComment && commentBlock}
+  //     </div>
+  //   );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
